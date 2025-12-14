bbf0629e170ccad5b0c48bcfc8ac9309c23050d1
wayuto <4e07910@gmail.com>
2025-12-14 20:35:50 +0800

    bug fix: std: array(who modified my code?)

--- gos-std/src/array.rs
+++ gos-std/src/array.rs
@@ -4,12 +4,12 @@ const MAX_RANGE_LEN: usize = 1024;
 static mut RANGE_BUFFER_WITH_LEN: [isize; MAX_RANGE_LEN + 1] = [0; MAX_RANGE_LEN + 1];
 
 #[unsafe(no_mangle)]
-pub extern "C" fn range(n: isize) -> *mut isize {
-    if n <= 0 {
+pub extern "C" fn range(start: isize, end: isize) -> *mut isize {
+    if end <= start {
         return ptr::null_mut();
     }
     unsafe {
-        let mut len = n as usize;
+        let mut len = (end - start) as usize;
 
         let base_ptr = &mut RANGE_BUFFER_WITH_LEN[0] as *mut isize;
 
@@ -22,7 +22,7 @@ pub extern "C" fn range(n: isize) -> *mut isize {
         let data_ptr = base_ptr.add(1);
 
         for i in 0..len {
-            *data_ptr.add(i) = i as isize;
+            *data_ptr.add(i) = start + i as isize;
         }
 
         base_ptr
